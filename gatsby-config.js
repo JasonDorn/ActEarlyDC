0aa0ebf1ac45018791bb19872dcd2a4c21522f32
Jason Dorn <apollo701@gmail.com>
2021-03-07 09:34:59 -0500

    Update gatsby/package.json config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Learn The Signs Act Early DC',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Website built to help obtain recognize developmental disabilities in children ages 0-5',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
